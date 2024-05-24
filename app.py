47c65c2ae4e5f0e0af467ec3ee64328dc839085b
mandazavee22 <mandazavimbainashe3@gmail.com>
2024-05-24 17:31:09 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ import streamlit as st
 import requests
 import sqlite3
 import re
-import folium
 from streamlit_folium import folium_static
 import matplotlib.pyplot as plt
 import plotly.graph_objects as go
